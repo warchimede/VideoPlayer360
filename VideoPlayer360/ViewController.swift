f1c0ecb314fb842b4b962b212342aecf97b8ba4c
William Archimede <william.archimede@hoodbrains.com>
2017-11-15 15:51:15 +0100

    Initial Commit

---
+++ VideoPlayer360/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoPlayer360
+//
+//  Created by William Archimède on 15/11/2017.
+//  Copyright © 2017 William Archimede. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
